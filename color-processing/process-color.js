63f0f3c95e8b4cbce979d38e3f2f3cb32df79d72
Estelle Kim <iamestellekim@gmail.com>
2024-10-05 00:51:24 -0400

    outline for score calculations

--- color-processing/process-color.js
+++
@@ -1,9 +0,0 @@
-//input: 2 items output: compatibility score
-
-//receive 2 arrays of 3 hsl coded colors
-
-//1. plot both on circle & get degrees
-
-//2. use weather, style, and occasion to figure out which criteria to use
-
-//3. calculate compatibility score from 0 to 1 based on the used criteria, add it to 
